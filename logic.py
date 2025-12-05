f9f643e4a6a9f1e19d169ed8ef4a0c79132eaead
lpdodgers <ferries.calm4r@icloud.com>
2025-12-04 18:32:12 -0600

    GUI Interface, with small logic going

--- logic.py
+++ logic.py
@@ -1,24 +1,30 @@
-from PyQt6.QtWidgets import QMainWindow
-
+from PyQt6.QtWidgets import *
 from Vote_Menu_GUI import *
-from candidate_menu_gui import *
 import csv
 
-class Logic(Ui_GUI_Vote_Menu, Ui_Dialog_Candidate_Menu):
+class Logic(QMainWindow, Ui_GUI_Vote_Menu):
     def __init__(self):
-        Ui_GUI_Vote_Menu.__init__(self)
-        Ui_Dialog_Candidate_Menu.__init__(self)
+        super().__init__()
         self.setupUi(self)
         self.setWindowTitle("VOTE MENU")
 
-    def vote(self):
-        in_label = str(self.line_v_x.text().strip()).lower()
+    def vote_id(self):
+        id_list = []
+        try:
+            id = int(self.lineEdit_voter_id.text())
+        except(ValueError):
+            self.error()
+            self.label_message.setText("Please enter a valid Voter ID")
+            self.lineEdit_voter_id.setText("")
+        if id in id_list:
+            self.error()
+            self.label_message.setText("ID already voted")
+            self.lineEdit_voter_id.setText("")
 
-        if in_label is not None:
-            if in_label == "x":
-                ##Close window
-                pass
-            elif in_label == "v":
-                ##Close Vote_menu
-                ##Open Candidate_Menu
-                pass
+    def vote(self):
+        candidate = self.dropBox_candidates
+
+
+
+    def error(self):
+        self.label_message.setStyleSheet("color: red")
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from logic import *
 
 def main():
-    pass
+    application = QApplication([])
+    window = Logic()
+    window.show()
+    application.exec()
 
 if __name__ == '__main__':
     main()
